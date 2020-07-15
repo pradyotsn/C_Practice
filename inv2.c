39ed7fb685432e8fb8cb0dd47ff953b70ccc47f2
Pradyot S Nadig <pradyotsn@gmail.com>
2020-07-15 11:12:17 +0530

    Update inv2.c

--- inv2.c
+++ inv2.c
@@ -15,7 +15,7 @@ struct inventory in[5];
 
 //printf("Enter Records of 5 Inventory Items");
 
-// Details of 1st iteem
+// Details of 1st item
 strcpy(in[0].Item_name,"abc");
 
 
@@ -26,16 +26,18 @@ strcpy(in[0].Warehouse_location,"Jayanagar");
 
 strcpy(in[0].Branch_name,"XYZ");
 
-strcpy(in[0].City,"Blorefak");
+strcpy(in[0].City,"Blore");
 
-
+// Like this do it for next 4 items,  change the array numbers accordingly
 
 
 printf("\nInventory Information List:\n");
 printf("Item_name \t Item_number \t Warehouse_location \t Branch_name \t City\n");
 
+
+// change for loop from 1 to 5 later
 for(i=0;i<1;i++){
-printf("%s \t\t %d \t\t %s \t\t %s kk \t\t %s \n",in[i].Item_name,in[i].Item_number,in[i].Warehouse_location,in[i].Branch_name,in[i].City);
+printf("%s \t\t %d \t\t %s \t\t %s \t\t %s \n",in[i].Item_name,in[i].Item_number,in[i].Warehouse_location,in[i].Branch_name,in[i].City);
 }
    return 0;
 }
